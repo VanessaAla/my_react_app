96043b3def057312c142d786e6185f5c7e589824
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-02-24 13:38:22 +0100

    add an OnClick button

--- src/components/LikeCounter.js
+++ src/components/LikeCounter.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react"; // <- note the added import of useState
+import React, { useState } from "react";
 
 export default function LikeCounter() {
   const initial_numLikes = 0;
-  const [numLikes, set_numLikes] = useState(initial_numLikes); // <- using state!
+  const [numLikes, set_numLikes] = useState(initial_numLikes);
 
-  console.log("what are these?", numLikes, set_numLikes);
+  const increment = () => {
+    console.log("Yes, clicked! Current number of likes:", numLikes);
+    set_numLikes(numLikes + 1);
+  };
 
   return (
     <div>
       <p>
         This post has <b>{numLikes}</b> likes!
+        <button onClick={increment}>Like</button>
       </p>
     </div>
   );
